769a3ded27fe1f6e04be4a3b4ef66d7257e25b33
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-07-20 12:08:11 -0700

    images and js added to complete project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const path = require('path');
+
+//Import routes
+var routes = require('./routes')
+
+app.use('/', routes)
+
+//View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+//Static middleware
+app.use(express.static('public'))
+
+// 404 error handler
+app.use((req, res, next) => {
+  err = new Error('Page not found');
+  err.status = 404;
+  err.message = `${err.status}`;
+  next(err);
+});
+
+//Global error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    console.log(err);
+  }
+  if (err.status === 404) {
+    res.status = 404;
+    res.render('error', { err });
+  } else {
+    err.message = err.message || `${err.status}`;
+    err.status = (err.status || 500);
+    res.render('error', { err });
+  }
+});
+
+app.listen(port, function(){
+  console.log(`${port}`)
+});
---
+++ routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const {projects} = require('./data/data.json');
+
+/**
+ * GET home page
+ */
+router.get( '/', ( req, res, next) => {
+  res.render('index', {projects})
+});
+
+
+//About page
+router.get('/about',(req, res) => {
+res.render('about')
+})
+
+//Get individual project page
+router.get('/projects/:id', (req, res, next)=>{
+  const projectId = req.params.id
+  const project = projects.find(({id}) => id === +projectId);
+
+  if(project){
+    res.render('project', {project})
+  } else {
+    throw new Error('Error')
+  }
+  });
+
+//exports router
+module.exports = router;
