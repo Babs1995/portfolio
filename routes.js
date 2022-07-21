ce96bdbe238bdff105f4394fe55e3c78b8c23fc9
Babs1995 <106346978+Babs1995@users.noreply.github.com>
2022-07-21 11:54:42 -0700

    chnages

--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const router = express.Router();
 const {projects} = require('./data/data.json');
@@ -6,7 +8,7 @@ const {projects} = require('./data/data.json');
  * GET home page
  */
 router.get( '/', ( req, res, next) => {
-  res.render('index', {projects})
+  res.render('index', {project})
 });
 
 
